9a0de17b6f8d9a83f5e49ae7dce53405be0e5658
Suraj Sharma <suraj.sharma@quillbot.com>
2024-12-10 13:11:07 +0530

    added basic ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,52 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import {
+  Box,
+  Button,
+  Container,
+  Paper,
+  TextareaAutosize,
+  Typography,
+  styled,
+} from "@mui/material";
+
+const TextArea = styled(TextareaAutosize)(({ theme }) => ({
+  width: "100%",
+  fontSize: "16px",
+  border: "none",
+  color: theme.palette.text.secondary,
+  fontFamily: "inter",
+  resize: "none",
+  padding: 8
+}));
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <Container>
+      <Typography
+        variant="h1"
+        fontSize={20}
+        fontWeight={600}
+        padding={2}
+        textAlign="center"
+      >
+        Text To Speech Converter
+      </Typography>
+      <Paper
+        elevation={3}
+        sx={{ padding: 2, display: "flex", flexDirection: "column", gap: 2 }}
+      >
+        <TextArea
+          minRows={10}
+          defaultValue="Good day, how are you?"
+          placeholder="Enter some text to convert to speech"
         />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <Box>
+          <Button variant="contained" sx={{ textTransform: "none" }}>
+            Transform into speech
+          </Button>
+        </Box>
+      </Paper>
+    </Container>
   );
 }
